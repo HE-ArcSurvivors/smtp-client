89de7d17ed11601211e5dfafbb3e4e9de48a0dbe
rocla <romain.claret@rocla.ch>
2014-06-12 12:50:18 +0200

    made a starting main :)

--- SMTP/main.c
+++ SMTP/main.c
@@ -1,8 +1,25 @@
+/*
+ * SMTP Client
+ * Authors: Roman Yakovenko & Benjamin Margueron
+ *
+ * Created on June 12, 2013
+ * Last Update on potato day
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
+//#include <netdb.h>
+//#include <unistd.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    printf("Hello world! BANANA COOKIE :)\n");
+    char* source_email = "roman.yakovenko@he-arc.ch";
+    char* destination_email = "benjamin.margueron@he-arc.ch";
+    char* subject = "test";
+    char* file = "data.txt";
+    char* hostname = "localhost";
+
+    printf("Hello world! BANANA COOKIE for the win :)\n");
     return 0;
 }
