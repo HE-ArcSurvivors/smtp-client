d22afd61253a8935afa7de552befefd58055a53d
rocla <romain.claret@rocla.ch>
2014-06-12 13:03:34 +0200

    prise en charge des arguments dans l'ordre du cahier des taches

--- SMTP/main.c
+++ SMTP/main.c
@@ -12,13 +12,34 @@
 //#include <netdb.h>
 //#include <unistd.h>
 
+char* sender = "roman.yakovenko@he-arc.ch";
+char* receiver = "benjamin.margueron@he-arc.ch";
+//char* default_sender = "benjamin.margueron@he-arc.ch";
+//char* default_reciever = "roman.yakovenko@he-arc.ch";
+char* subject = "test";
+char* message = "data";
+char* hostname = "localhost";
+
 int main(int argc, char** argv)
 {
-    char* source_email = "roman.yakovenko@he-arc.ch";
-    char* destination_email = "benjamin.margueron@he-arc.ch";
-    char* subject = "test";
-    char* file = "data.txt";
-    char* hostname = "localhost";
+    for (int i = 1; i < argc; i++)
+        switch (i) {
+            case 1:
+                sender = argv[i];
+                break;
+            case 2:
+                subject = argv[i];
+                break;
+            case 3:
+                message = argv[i];
+                break;
+            case 4:
+                hostname = argv[i];
+                break;
+            case 5:
+                receiver = argv[i];
+                break;
+        }
 
     printf("Hello world! BANANA COOKIE for the win :)\n");
     return 0;
